5dddc1224a1540c14ea7fb1cf521cc3a5e042aa5
vianey <vianeylinares@gmail.com>
2023-11-13 12:12:05 -0800

    Added ContentAreaHeight helper.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Routing } from "./router/Routing";
+import { ContentAreaHeight } from "./components/helpers/ContentAreaHeight";
 
 function App() {
+  ContentAreaHeight();
   return (
     <div className="wrapper">
       <Routing />
