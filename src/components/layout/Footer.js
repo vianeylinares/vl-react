e4b11d33f6bace3b1004f48be5e8c8d44b730902
vianey <vianeylinares@gmail.com>
2024-04-06 11:34:14 -0700

    Added title attribute to social media links.

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -8,16 +8,16 @@ export const Footer = () => {
   return (
     <div className="footer">
       <div className='socials'>
-                <Link to="https://github.com/vianeylinares/" target="_blank" rel="nofollow">
+                <Link to="https://github.com/vianeylinares/" target="_blank" rel="nofollow" title="Vianey's GitHub profile">
                     <FontAwesomeIcon icon={faGithub} />
                 </Link>
-                <Link to="mailto:me@vianeylinares.com" target="_blank" rel="nofollow">
+                <Link to="mailto:me@vianeylinares.com" target="_blank" rel="nofollow" title="Vianey's e-mail">
                     <FontAwesomeIcon icon={faEnvelope} />
                 </Link>
-                <Link to="https://linkedin.com/in/vianeylinares/" target="_blank" rel="nofollow">
+                <Link to="https://linkedin.com/in/vianeylinares/" target="_blank" rel="nofollow" title="Vianey's LinkedIn profile">
                     <FontAwesomeIcon icon={faLinkedin} />
                 </Link>
-                <Link to="https://twitter.com/vl_devs/" target="_blank" rel="nofollow">
+                <Link to="https://twitter.com/vl_devs/" target="_blank" rel="nofollow" title="Vianey's Twitter profile">
                     <FontAwesomeIcon icon={faTwitter} />
                 </Link>
             </div>
