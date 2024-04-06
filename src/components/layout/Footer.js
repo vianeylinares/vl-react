66bc83be79747710ebfc9d943b6af903b9f2903d
vianey <vianeylinares@gmail.com>
2024-04-06 11:07:40 -0700

    Added rel='nofollow' attribute to social media links. Also removed commented medium link and icon reference.

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,27 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub, faMedium, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 export const Footer = () => {
   return (
     <div className="footer">
       <div className='socials'>
-                <Link to="https://github.com/vianeylinares/" target="_blank">
+                <Link to="https://github.com/vianeylinares/" target="_blank" rel="nofollow">
                     <FontAwesomeIcon icon={faGithub} />
                 </Link>
-                <Link to="mailto:me@vianeylinares.com" target="_blank">
+                <Link to="mailto:me@vianeylinares.com" target="_blank" rel="nofollow">
                     <FontAwesomeIcon icon={faEnvelope} />
                 </Link>
-                <Link to="https://linkedin.com/in/vianeylinares/" target="_blank">
+                <Link to="https://linkedin.com/in/vianeylinares/" target="_blank" rel="nofollow">
                     <FontAwesomeIcon icon={faLinkedin} />
                 </Link>
-                <Link to="https://twitter.com/vl_devs/" target="_blank">
+                <Link to="https://twitter.com/vl_devs/" target="_blank" rel="nofollow">
                     <FontAwesomeIcon icon={faTwitter} />
                 </Link>
-                {/*<Link to="https://medium.com/@the-chaos-developer/" target="_blank">
-                    <FontAwesomeIcon icon={faMedium} />
-                </Link>*/}
             </div>
     </div>
   )
