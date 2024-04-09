f740fd2887d7576a71b56aa8becc5584e2c25b7c
vianey <vianeylinares@gmail.com>
2024-04-08 18:23:27 -0700

    Added descriptive text to Home page content section.

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ export const Home = () => {
         <div className='welcome'>
           <h2>Welcome</h2>
           <p>
-            Hi, I am Vianey. I am passionate about technology, web development and solving problems.
+            Hi, I am Vianey. I am passionate about technology, web development and solving problems. Currently, I work as a freelance full-stack WordPress developer and website fixer.
           </p>
         </div>
       </div>
